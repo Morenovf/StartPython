c311a9573410dd63f25189a97c48f5b3f1697a73
JonPiniesEcheguren <jon.pinies.ech@gmail.com>
2023-04-11 13:47:52 +0200

    Con funciones principales

--- Ejercicio11_20Python.py
+++ Ejercicio11_20Python.py
@@ -1,10 +1,11 @@
 # 11. Crea un algoritmo que calcule el factorial de un número entero.
-print("Introduzca un número entero:")
-numero = input()
-numero = int(numero)
-i = 1
-factorial=1
-while i <=numero:
-    factorial *=  i
-    i += 1
-print("El factorial de", numero, "es", factorial)
+def principal():
+    print("Introduzca un número entero:")
+    numero = input()
+    numero = int(numero)
+    i = 1
+    factorial=1
+    while i <=numero:
+        factorial *=  i
+        i += 1
+    print("El factorial de", numero, "es", factorial)
