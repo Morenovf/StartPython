72c46aa66380fd832f6af51e4b37fdfdc07c9eff
JonPiniesEcheguren <jon.pinies.ech@gmail.com>
2023-04-12 13:01:47 +0200

    Cambios

--- Ejercicios/Ejercicio20capicua.py
+++ Ejercicios/Ejercicio20capicua.py
@@ -1,8 +1,20 @@
 # 20. Crea un algoritmo que determine si un número es capicúa o no.
 def principal():
-    print("Introduzca un número entero:")
-    num = input()
+    x = 0
     i = 0
+    while x == 0:
+        print("Introduzca un número entero:")
+        num = input()
+        if num.isdigit() and len(num)<=1:
+            print("El número no es capicúa")
+            break
+        try:
+            num = int(num)
+            x = 1
+        except:
+            print("Entrada incorrecta")
+            continue
+    num = str(num)
     while i < len(num):
         if num[i]==num[-i-1]:
             i += 1
@@ -10,5 +22,5 @@ def principal():
         else:
             print("El número no es capicúa")
             break
-    if i == len(num):
+    if i == len(num) and len(num)>1:
         print("El número es capicúa")
