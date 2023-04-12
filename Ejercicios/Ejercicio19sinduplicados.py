fd51a83961a6c85963998b02419cbd0786737dbd
JonPiniesEcheguren <jon.pinies.ech@gmail.com>
2023-04-12 13:16:54 +0200

    Con validaciones y corregido

--- Ejercicios/Ejercicio19sinduplicados.py
+++ Ejercicios/Ejercicio19sinduplicados.py
@@ -1,18 +1,18 @@
 # 19. Dada una lista de números enteros, crea un algoritmo que elimine los números duplicados de la lista.
-#def principal():
-num = "a"
-lista = list()
-while len(num)>0:
-    print("Introduzca un nuevo número entero. Enter para terminar.")
-    num = input()
-    try:
-        num = int(num)
-    except:
-        if num != "":
-            print("Entrada inválida")
-            continue
-    num = str(num)
-    if len(num)>0:
-        if num not in lista:
-            lista.append(num)
-print(lista)
+def principal():
+    num = "a"
+    lista = list()
+    while len(num)>0:
+        print("Introduzca un nuevo número entero. Enter para terminar.")
+        num = input()
+        try:
+            num = int(num)
+        except:
+            if num != "":
+                print("Entrada inválida")
+                continue
+        num = str(num)
+        if len(num)>0:
+            if num not in lista:
+                lista.append(num)
+    print(lista)
